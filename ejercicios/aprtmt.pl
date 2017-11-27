3d76d501aed97deb85f8d3ea966be1e252c26a10
Omar Mendo <alu0100845396@ull.edu.es>
2017-11-27 09:25:56 +0000

    Subiendo puzzle

--- ejercicios/aprtmt.pl
+++ ejercicios/aprtmt.pl
@@ -1,18 +1,33 @@
 /* Problema de asignación de apartamentos */
 
-1. Definir predicado "higher"
-2. Definir predicado "not_adyacent"
-3. Definir puzzle(L):-
-      permutation(L,[juan,antonio,ana,maria,luis]),
-      L = [Atico,Piso4,Piso3,Piso3,Bajo],
-      juan \== Atico,
-      antonio \== Bajo
-      ana \== Atico
-      ana \== Bajo
-      higher(luis,antonio, L),
-      not_adyacent(maria,ana, L),
-      not_adyacent(ana,antonio, L).
+member(X,[X|T]).
+member(X,[_|T]):-
+   member(X,T).
 
+higher(X,Y,[X|T]):-	
+   member(Y,T).
+higher(X,Y,[_|T]):-	
+   higher(X,Y,T).
 
-      ? puzzle(L).
-      ? puzzle([A,B,C,D,E]).
+not_adjacent(X,Y,[X,Z|T]):-
+   Z \== Y,
+   member(Y,T).
+
+not_adjacent(X,Y,[Y,Z|T]):-
+   Z \== X,
+   member(X,T).
+
+not_adjacent(X,Y,[_|T]):-
+   not_adjacent(X,Y,T).
+	
+puzzle(L):-
+   permutation(L,[juan,antonio,ana,maria,luis]),
+   L = [Atico,Piso4,Piso3,Piso2,Bajo],
+
+   juan \== Atico,
+   antonio \== Bajo,
+   ana \== Atico,
+   ana \== Bajo,
+   higher(luis,antonio, L),
+   not_adjacent(maria,ana, L),
+   not_adjacent(ana,antonio, L).
